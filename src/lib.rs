75cc6c9d7f284262ad85f4ee30d2758df245b367
sanprasirt <sanprasirt>
2026-06-23 08:27:38 +0700

    feat: add subscription feature with form handling and database integration
    - Updated Cargo.toml to include dependencies for serde, config, and sqlx.
    - Implemented configuration loading from a YAML file for application settings.
    - Created a new route for subscriptions, handling form data and saving to the database.
    - Added health check route for server status verification.
    - Introduced database initialization script for setting up PostgreSQL with Docker.
    - Enhanced tests to cover subscription functionality and validate responses.

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,36 @@
 use actix_web::{App, HttpResponse, HttpServer, Responder, web};
 use actix_web::dev::Server;
+use std::net::TcpListener;
+
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String
+}
+
+// Let's start simple
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+// Notice the different signature!
+// Starts the Actix web server and exposes the health check endpoint.
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
         })
-        .bind("127.0.0.1:8000")?
+        .listen(listener)?
         .run();
-
+    // No. .await here!
     Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-use zero2prod::run;
+use std::net::TcpListener;
+
+use zero2prod::{configuration::get_configuration, run};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Faild to read configuration!");
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await in our Server
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
